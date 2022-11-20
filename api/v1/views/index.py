fa7990bc7fba1298620b80d8d7766c2243415367
Ajayi <ajayidavies7@gmail.com>
2022-11-20 08:47:17 -0800

    data added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,28 @@
 ''' Use Blueprint instance '''
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def return_status():
     ''' returns status code'''
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', strict_slashes=False)
+def obj_count():
+    ''' return count of objects '''
+    amenity = storage.count(Amenity)
+    city = storage.count(City)
+    review = storage.count(Review)
+    state = storage.count(State)
+    user = storage.count(User)
+    object_dict = {'amenities': amenity, 'city': city, 'review': review,
+                   'state': state, 'user': user}
+    return jsonify(object_dict)
